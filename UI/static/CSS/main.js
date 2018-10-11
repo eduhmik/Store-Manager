41b681dd765e554cb849ebcb41981419ebb2bd11
Eduhmik <edwinkimaita78@gmail.com>
2018-10-11 08:40:49 +0300

    [Feature #161046610] Created a base.html file

---
+++ UI/static/CSS/main.js
@@ -0,0 +1,19 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+} 
+
+function myFunction(x) {
+    x.classList.toggle("change");
+} 
+
+function todo(){
+    openNav(); closeNav();
+}
